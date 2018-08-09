43df7a74194d5f02c1f38a698c9a7569ad5335aa
jose daniel <josedanielf9@gmail.com>
2018-08-09 15:30:48 -0400

    servicio general

--- public/agil client/www/js/agil/servicios/serviciosGeneral.js
+++ public/agil client/www/js/agil/servicios/serviciosGeneral.js
@@ -1292,7 +1292,7 @@ angular.module('agil.servicios')
 					if (venta.configuracion.tamanoPapelFactura.nombre_corto == Diccionario.FACT_PAPEL_OFICIO) {
 						itemsPorPagina = 19;
 					} else if (venta.configuracion.tamanoPapelFactura.nombre_corto == Diccionario.FACT_PAPEL_CARTA) {
-						itemsPorPagina = 16;
+						itemsPorPagina = 19;
 					} else if (venta.configuracion.tamanoPapelFactura.nombre_corto == Diccionario.FACT_PAPEL_MEDIOOFICIO) {
 						itemsPorPagina = 3;
 					}
