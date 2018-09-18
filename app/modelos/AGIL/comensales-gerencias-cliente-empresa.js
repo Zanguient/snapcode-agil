6046aada90aa68dce7f92b4894013d5c972303be
jose daniel <josedanielf9@gmail.com>
2018-09-18 10:45:45 -0400

    comensalesa y estados financieros

--- app/modelos/AGIL/comensales-gerencias-cliente-empresa.js
+++ app/modelos/AGIL/comensales-gerencias-cliente-empresa.js
@@ -35,7 +35,7 @@ module.exports = function (sequelize, Sequelize) {
 		});
 
 	GerenciasClienteEmpresa.sync().then(function () {
-		
+	
 	});
 	return GerenciasClienteEmpresa;
 }
--- public/agil client/www/js/estadosFinancierosAgil/controladores/ControladorComprobanteDiario.js
+++ public/agil client/www/js/estadosFinancierosAgil/controladores/ControladorComprobanteDiario.js
@@ -3348,6 +3348,7 @@ angular.module('agil.controladores')
 
 		$scope.generarExcelComprobanteDiario =function(){
 			$scope.periodo = $scope.configuracionImpresion.tipoPeriodo.nombre;
+			$scope.cuenta = $scope.configuracionImpresion.tipo_cuenta.nombre;
 			var meses = new Array(["Enero",1],["Febrero",2],["Marzo",3],["Abril",4],["Mayo",5],["Junio",6]
 			,["Julio",7],["Agosto",8],["Septiembre",9],["Octubre",10],["Noviembre",11],
 				["Diciembre",12]);
