0c4e886264ddfc816ca3d506f42b6f3e9d534f3e
jose daniel <josedanielf9@gmail.com>
2018-10-19 14:52:06 -0400

    gtm despacho

--- app/rutas/rutas-gtm-despacho.js
+++ app/rutas/rutas-gtm-despacho.js
@@ -1229,6 +1229,12 @@ module.exports = function (router, ensureAuthorizedAdministrador, fs, forEach, j
 				}
 				GtmDespachoDetalle.findAll({
 					where: condicionDetalleDespacho,
+					attributes: [
+						[ sequelize.literal(
+							'COALESCE(servicio_transporte, 0) / COALESCE(cantidad, 0)'
+						), 'servicio_transporte'
+						]
+					],
 					include: [{
 						model: GtmDespacho, as: 'despacho',
 						where: condicionDespacho,
